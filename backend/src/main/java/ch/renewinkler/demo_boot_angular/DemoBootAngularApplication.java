36c20812558823ed6426319d496e9175186e2372
René Winkler <rene.winkler@xovis.com>
2017-02-10 14:38:17 +0100

    first commit

---
+++ backend/src/main/java/ch/renewinkler/demo_boot_angular/DemoBootAngularApplication.java
@@ -0,0 +1,12 @@
+package ch.renewinkler.demo_boot_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootAngularApplication.class, args);
+	}
+}
