a4907bf286c0453233610d24bd14d7369fefbda5
René Winkler <rene.winkler@xovis.com>
2017-02-17 10:46:45 +0100

    add language in liquibase

--- backend/src/main/java/ch/renewinkler/demo_boot_angular/language/LanguageService.java
+++ backend/src/main/java/ch/renewinkler/demo_boot_angular/language/LanguageService.java
@@ -1,9 +1,11 @@
 package ch.renewinkler.demo_boot_angular.language;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class LanguageService {
 
     @Autowired
